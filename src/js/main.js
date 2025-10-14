0fde375774a9918c247cd3336c50cc6202e13e0b
Nyashes <118954257+GhostyBroski@users.noreply.github.com>
2025-10-14 11:34:00 -0600

    Built NPS 4

--- src/js/main.js
+++ src/js/main.js
@@ -1,119 +1,29 @@
-import { getParkData } from "./parkService.mjs";
-
-const parkData = getParkData();
-
-function parkInfoTemplate(info) {
-  return `<a href="/" class="hero-banner__title">${info.name}</a>
-  <p class="hero-banner__subtitle">
-    <span>${info.designation}</span>
-    <span>${info.states}</span>
-  </p>`;
-}
-
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
-
-document.querySelector("head > title").textContent = parkData.fullName;
-
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
-
-document.querySelector(".hero-banner__content").innerHTML =
-  parkInfoTemplate(parkData);
-
-
-
-const parkInfoLinks = [
-  {
-    name: "Current Conditions &#x203A;",
-    link: "conditions.html",
-    image: parkData.images[2].url,
-    description:
-      "See what conditions to expect in the park before leaving on your trip!"
-  },
-  {
-    name: "Fees and Passes &#x203A;",
-    link: "fees.html",
-    image: parkData.images[3].url,
-    description: "Learn about the fees and passes that are available."
-  },
-  {
-    name: "Visitor Centers &#x203A;",
-    link: "visitor_centers.html",
-    image: parkData.images[9].url,
-    description: "Learn about the visitor centers in the park."
-  }
-];
-
-
-
-function setHeaderInfo(data) {
-  // insert data into disclaimer section
-  const disclaimer = document.querySelector(".disclaimer > a");
-  disclaimer.href = data.url;
-  disclaimer.innerHTML = data.fullName;
-  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
-  document.querySelector("head > title").textContent = data.fullName;
-  // set the banner image
-  document.querySelector(".hero-banner > img").src = data.images[0].url;
-  // use the template function above to set the rest of the park specific info in the header
-  document.querySelector(".hero-banner__content").innerHTML =
-    parkInfoTemplate(data);
-}
-
-setHeaderInfo(parkData);
-
-function mediaCardTemplate(info) {
-  return `<div class="media-card">
-  <a href="${info.link}">
-  <img src="${info.image}" alt="${info.name}" class="media-card__img">
-  <h3 class="media-card__title">${info.name}</h3>
-  </a>
- <p>${info.description}</p>
-   </div>`;
-}
-function getMailingAddress(addresses) {
-  const mailing = addresses.find((address) => address.type === "Mailing");
-  return mailing;
-}
-function getVoicePhone(numbers) {
-  const voice = numbers.find((number) => number.type === "Voice");
-  return voice.phoneNumber;
-}
-function footerTemplate(info) {
-  const mailing = getMailingAddress(info.addresses);
-  const voice = getVoicePhone(info.contacts.phoneNumbers);
-
-  return `<section class="contact">
-  <h3>Contact Info</h3>
-  <h4>Mailing Address:</h4>
-  <div><p>${mailing.line1}<p>
-  <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p></div>
-  <h4>Phone:</h4>
-  <p>${voice}</p>
-  </section>`;
-}
-
-
+import "../css/style.css"; // we can do this because we are using Vite...
+import "../css/home.css";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 
 function setParkIntro(data) {
-  const introElement = document.querySelector(".intro");
-  introElement.innerHTML = `<h1>${data.fullName}</h1>
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
   <p>${data.description}</p>`;
 }
 
 function setParkInfoLinks(data) {
-  const infoElement = document.querySelector(".info");
+  const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
   const html = data.map(mediaCardTemplate);
-  infoElement.innerHTML = html.join("");
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-function setFooter(data) {
-  const footerElement = document.querySelector("#park-footer");
-  footerElement.innerHTML = footerTemplate(data);
+async function init() {
+  const parkData = await getParkData();
+  const links = getInfoLinks(parkData.images);
+  setHeaderFooter(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
 }
 
-setHeaderInfo(parkData);
-setParkIntro(parkData);
-setParkInfoLinks(parkInfoLinks);
-setFooter(parkData);
+init();
